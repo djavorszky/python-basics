6661c53b62240c7c08a610329d2c7d974ddf17ad
Daniel Javorszky <daniel.javorszky@liferay.com>
2019-06-04 14:18:32 +0200

    Fix indentation

--- 29.py
+++ 29.py
@@ -1,7 +1,7 @@
 criticalTickets = ticket_persistence.find_opened_only_critical_tickets(region_persistence.find_region_id('Hungary'), True)
 
-	with criticalTickets:
-        for criticalTicket in criticalTickets:
+with criticalTickets:
+    for criticalTicket in criticalTickets:
         actualTicketEntryId = criticalTicket['ticketEntryId']
 
         try:
@@ -10,6 +10,6 @@ criticalTickets = ticket_persistence.find_opened_only_critical_tickets(region_pe
             mainLogger.log("Skipping " + str(ticket['lesaKey']) + " as no primary worker found")
             continue
 
-		statusDocument.add_rush_ticket(team, criticalTicket['lesaKey'])
+        statusDocument.add_rush_ticket(team, criticalTicket['lesaKey'])
 
-    statusDocument.set_create_date(get_now_date())
+        statusDocument.set_create_date(get_now_date())
